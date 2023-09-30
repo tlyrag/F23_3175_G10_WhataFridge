a7363100ea860567b96fd2f89529f42bfc40b3a2
Thiago Lyra Ganem <tlyraganem@gmail.com>
2023-09-30 11:45:37 -0700

    Fixing Retrofit issues and making API Work

--- WhatAFridge/app/src/main/java/com/douglas/whatafridge/MainActivity.java
+++ WhatAFridge/app/src/main/java/com/douglas/whatafridge/MainActivity.java
@@ -2,41 +2,31 @@ package com.douglas.whatafridge;
 
 import androidx.appcompat.app.AppCompatActivity;
 import com.douglas.whatafridge.Controller.*;
-import com.douglas.whatafridge.Model.*;
+//import com.douglas.whatafridge.Model.*;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.TextView;
 
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
-
+    SpoonacularController api = new SpoonacularController();
+    TextView txtTitle;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         try{
-            SpoonacularController api = RetrofitClient.getInstance().getSpoonacularController();
-            Call<Recipe> call = api.getRecipesByIngredients("chicken", 5, true, 1, true);
-
-            call.enqueue(new Callback<Recipe>() {
-                @Override
-                public void onResponse(Call<Recipe> call, Response<Recipe> response) {
-                    //Log.d("WTF App",response.body().toString());
-                }
-
-                @Override
-                public void onFailure(Call<Recipe> call, Throwable t) {
-
-                }
-            });
-
-
+        api.getRecipeByIngredient(this,"salmon",1);
+        txtTitle = findViewById(R.id.TextViewTitle);
+        String RecipeTitle = api.recipesByIngredientList.get(0).title;
+        txtTitle.setText(RecipeTitle);
 
         } catch(Exception err) {
-            Log.d("WTF App", "Api error");
+            Log.d("WTF App", "Api error"+ err.getMessage());
         }
 
 
--- WhatAFridge/app/src/main/java/com/douglas/whatafridge/Model/Recipe.java
+++
@@ -1,5 +0,0 @@
-package com.douglas.whatafridge.Model;
-
-public class Recipe {
-    private String title;
-}
