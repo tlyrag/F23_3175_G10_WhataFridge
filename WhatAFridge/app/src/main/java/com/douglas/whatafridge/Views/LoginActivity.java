2e78cf81d3a757cf1ed1594f927c2e0201756717
Nami Kim <ironjaycee@gmail.com>
2023-11-07 20:34:13 -0800

    add sending email function
    add sending email function
    and changes a bit

--- WhatAFridge/app/src/main/java/com/douglas/whatafridge/Views/LoginActivity.java
+++ WhatAFridge/app/src/main/java/com/douglas/whatafridge/Views/LoginActivity.java
@@ -5,16 +5,22 @@ import androidx.activity.result.ActivityResultCallback;
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.IntentSenderRequest;
 import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.MainThread;
 import androidx.annotation.NonNull;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.os.StrictMode;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
+import com.douglas.whatafridge.Controller.MailController;
 import com.douglas.whatafridge.R;
 import com.google.android.gms.auth.api.identity.BeginSignInRequest;
 import com.google.android.gms.auth.api.identity.BeginSignInResult;
@@ -29,6 +35,8 @@ import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
+import javax.mail.MessagingException;
+
 public class LoginActivity extends WFTemplate {
     SignInClient oneTapClient;
     BeginSignInRequest signUpRequest;
@@ -38,35 +46,39 @@ public class LoginActivity extends WFTemplate {
 
     Button btnLoginWithGoogle;
     Button btnLoginWithoutGoogle;
+    EditText editTextUserId;
+    EditText editTextPassword;
+
+
+    String Userid;
+    String Password;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        //simple SHA
-        byte[] message = "ddd".getBytes(StandardCharsets.UTF_8);
-        MessageDigest md = null;
-        try {
-            md = MessageDigest.getInstance("SHA-256");
-        } catch (NoSuchAlgorithmException e) {
-            throw new RuntimeException(e);
+        //after login successfully, it is stored in the device and use again.
+        SharedPreferences preferences = this.getPreferences(Context.MODE_PRIVATE);
+        Userid = preferences.getString(getString(R.string.txtUserid),"");
+        Password = preferences.getString(getString(R.string.txtPassword),"");
+
+        if(!Userid.equals("")){
+            //check the information with Database
+
         }
-        byte[] digest = md.digest(message);
-        String text = new String(digest, StandardCharsets.UTF_8);
-
-
 
         //hide actionbar
         if (getSupportActionBar() != null) {
             getSupportActionBar().hide();
         }
 
-
-        //google one tap login
         btnLoginWithGoogle = findViewById(R.id.btnLoginWithGoogle);
         btnLoginWithoutGoogle = findViewById(R.id.btnLoginWithoutGoogle);
 
+        //google one tap login
         oneTapClient = Identity.getSignInClient(this);
+
+        //send Client Id to OAuth 2.0 server
         signUpRequest = BeginSignInRequest.builder()
                 .setGoogleIdTokenRequestOptions(BeginSignInRequest.GoogleIdTokenRequestOptions.builder()
                         .setSupported(true)
@@ -77,8 +89,11 @@ public class LoginActivity extends WFTemplate {
                         .build())
                 .build();
 
+        //to get data from Activity
         ActivityResultLauncher<IntentSenderRequest> activityResultLauncher =
                 registerForActivityResult(new ActivityResultContracts.StartIntentSenderForResult(), new ActivityResultCallback<ActivityResult>() {
+
+                    //onActivityResult: main activity-> sub activity -> main activity
                     @Override
                     public void onActivityResult(ActivityResult result) {
                         if(result.getResultCode() == Activity.RESULT_OK){
@@ -88,20 +103,35 @@ public class LoginActivity extends WFTemplate {
                                 if (idToken !=  null) {
                                     // Got an ID token from Google. Use it to authenticate
                                     // with your backend.
-
                                     String email = credential.getId(); //email
-                                    Toast.makeText(LoginActivity.this, "Email : " +email , Toast.LENGTH_SHORT).show();
+                                    String password = credential.getPassword(); //password;
 
-                                    Log.d(TAG, "Got ID token.");
+                                    //simple SHA
+                                    byte[] message = password.getBytes(StandardCharsets.UTF_8);
+                                    MessageDigest md = null;
+                                    try {
+                                        md = MessageDigest.getInstance("SHA-256");
+                                    } catch (NoSuchAlgorithmException e) {
+                                        throw new RuntimeException(e);
+                                    }
+                                    byte[] digest = md.digest(message);
+                                    String shaPassword = new String(digest, StandardCharsets.UTF_8);
+
+                                    SharedPreferences.Editor editor = preferences.edit();
+                                    editor.putString(getString(R.string.txtUserid),email);
+                                    editor.putString(getString(R.string.txtPassword),shaPassword);
+
+                                    editor.apply();
+
                                 }
                             } catch (ApiException e) {
-                                // ...
                                 e.printStackTrace();
                             }
 
                         }
                     }
                 });
+
         btnLoginWithGoogle.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -127,7 +157,69 @@ public class LoginActivity extends WFTemplate {
                         });
             }
         });
+
+
         btnLoginWithoutGoogle.setOnClickListener(view -> {
+
+
+
+            /*
+
+            //sending email
+            StrictMode.setThreadPolicy(new StrictMode.ThreadPolicy.Builder()
+                                .permitDiskReads()
+                                .permitDiskWrites()
+                                .permitNetwork().build());
+
+            MailController mailController = new MailController();
+            String subject = "test";
+            String body = "test";
+            String recipients = "kimn25@student.douglascollege.ca";
+            try {
+                mailController.sendMail(subject,body,recipients);
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+
+
+
+            editTextUserId = findViewById(R.id.editTextUserId);
+            editTextPassword = findViewById(R.id.editTextPassword);
+
+            if(editTextUserId.getText().toString().isEmpty()){
+                Toast.makeText(this, "Enter User ID or User Email.", Toast.LENGTH_SHORT).show();
+            }else if(editTextPassword.getText().toString().isEmpty()){
+                Toast.makeText(this, "Enter the password.", Toast.LENGTH_SHORT).show();
+            }else{
+
+                String oriPassword = editTextPassword.getText().toString();
+                //simple SHA
+                byte[] message = oriPassword.getBytes(StandardCharsets.UTF_8);
+                MessageDigest md = null;
+                try {
+                    md = MessageDigest.getInstance("SHA-256");
+                } catch (NoSuchAlgorithmException e) {
+                    throw new RuntimeException(e);
+                }
+                byte[] digest = md.digest(message);
+                String shaPassword = new String(digest, StandardCharsets.UTF_8);
+
+                //compare to database
+
+
+
+                SharedPreferences.Editor editor = preferences.edit();
+                editor.putString(getString(R.string.txtUserid),editTextUserId.getText().toString());
+                editor.putString(getString(R.string.txtPassword),shaPassword);
+
+                editor.apply();
+
+            }
+
+
+
+
+             */
             startActivity(new Intent(LoginActivity.this,SearchRecipeActivity.class));
         });
     }
--- WhatAFridge/app/src/main/java/com/douglas/whatafridge/Views/ProfileActivity.java
+++ WhatAFridge/app/src/main/java/com/douglas/whatafridge/Views/ProfileActivity.java
@@ -2,15 +2,74 @@ package com.douglas.whatafridge.Views;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.os.StrictMode;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Switch;
+import android.widget.Toast;
 
+import com.douglas.whatafridge.Controller.MailController;
 import com.douglas.whatafridge.R;
 
+import java.util.Properties;
+
+import javax.mail.MessagingException;
+import javax.mail.Session;
+import javax.mail.internet.MimeMessage;
+
+
 public class ProfileActivity extends WFTemplate {
+
+    Button btnSave;
+
+    EditText editTextProfileUserId;
+    EditText editTextName;
+    EditText editTextAge;
+    EditText editTextHeight;
+    EditText editTextWeight;
+    Switch switchGlutenFree;
+    Spinner spinnerTypeVegetarian;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        editTextProfileUserId = findViewById(R.id.editTextProfileUserId);
+        editTextProfileUserId.setEnabled(false);
+
+        editTextName = findViewById(R.id.editTextName);
+        editTextAge = findViewById(R.id.editTextAge);
+        editTextHeight = findViewById(R.id.editTextHeight);
+        editTextWeight = findViewById(R.id.editTextWeight);
+        switchGlutenFree = findViewById(R.id.switchGlutenFree);
+        spinnerTypeVegetarian = findViewById(R.id.spinnerTypeVegetarian);
+
+        btnSave = findViewById(R.id.btnSave);
+
+        SharedPreferences preferences = this.getPreferences(Context.MODE_PRIVATE);
+        String userid = preferences.getString(getString(R.string.txtUserid),"");
+
+        //get information from database
+
+
+        //profile image control
+
+
+        //save
+        btnSave.setOnClickListener((View v) -> {
+
+
+
+            //save database
+        });
+
     }
 }
